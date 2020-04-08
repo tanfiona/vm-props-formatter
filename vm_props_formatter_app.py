5046137b34c0d4c0e6eb4cc4c75811b9cf1cc8c3
Fiona Tan <fiona.tan@charleskeith.com>
2020-04-08 18:19:29 +0800

    Complete all settings input and output links

--- vm_props_formatter_app.py
+++ vm_props_formatter_app.py
@@ -3,6 +3,7 @@ import base64
 import dash
 import datetime
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_table as dt
 import io
@@ -33,6 +34,31 @@ settings_path = 'settings/default_settings.json'
 outputs_path = 'outputs/'
 image_filename = 'settings/ck_logo.png'
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+hover_text = {
+    'settings-shape-main-header-row-text':
+        ['e.g. Row 8 where the main data row starts'],
+    'settings-shape-props-header-start-col-text':
+        ['e.g. Col 6 or F where the props column starts at BIG BAG STAND'],
+    'settings-shape-props-header-tally-first-text':
+        ['e.g. How many columns to tally from the first props col stand to find summary below'],
+    'settings-shape-no-summary-table-rows-text':
+        ['e.g. How many rows does summary table have'],
+    'settings-shape-summary-table-sum-row-text':
+        ['e.g. Row 2 in summary table contains the total values to check'],
+    'settings-shape-props-header-end-col-text': 
+        ['e.g. Last column (REMARKS) to drop that do not require to tallying'],
+    'settings-names-sheet-name-text':
+        [],
+    'settings-names-store-col-text':
+        [],
+    'settings-names-country-col-text':
+        [],
+    'settings-names-main-cols-text':
+        [],
+    'settings-names-entity-list-text':
+        []
+}
+entity_list = ['CKS','CKI','CKC']
 
 # Define upload default text
 upload_default_text = html.Div([
@@ -247,6 +273,24 @@ def generate_no_error_message():
     )
 
 
+def generate_hover_text(target_name):
+    return dbc.Tooltip(
+        hover_text[target_name],
+        target=target_name,
+        placement='top-start',
+        style={'width': '220pt',
+               'line-height': '150%',
+               'background-color': 'white',
+               'font-family': 'Helvetica',
+               'border': '2px solid #89CFF0',
+               'border-radius': '5px',
+               'padding-top': '5px',
+               'padding-bottom': '5px',
+               'padding-right': '5px',
+               'padding-left': '10px'}
+    )
+
+
 def generate_slider(id):
     m = 5 # multiple 
     max_v = 20 # max bar
@@ -439,10 +483,129 @@ app.layout = html.Div(
                                                         html.P(
                                                             id='settings-shape-main-header-row-text'
                                                         ),
+                                                        generate_hover_text(
+                                                            'settings-shape-main-header-row-text'
+                                                        ),
                                                         generate_slider(
                                                             'settings-shape-main-header-row-slider'
+                                                        ),
+                                                        html.P(
+                                                            id='settings-shape-props-header-start-col-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-shape-props-header-start-col-text'
+                                                        ),
+                                                        generate_slider(
+                                                            'settings-shape-props-header-start-col-slider'
+                                                        ),
+                                                        html.P(
+                                                            id='settings-shape-props-header-tally-first-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-shape-props-header-tally-first-text'
+                                                        ),
+                                                        generate_slider(
+                                                            'settings-shape-props-header-tally-first-slider'
+                                                        ),
+                                                        html.P(
+                                                            id='settings-shape-no-summary-table-rows-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-shape-no-summary-table-rows-text'
+                                                        ),
+                                                        generate_slider(
+                                                            'settings-shape-no-summary-table-rows-slider'
+                                                        ),
+                                                        html.P(
+                                                            id='settings-shape-summary-table-sum-row-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-shape-summary-table-sum-row-text'
+                                                        ),
+                                                        generate_slider(
+                                                            'settings-shape-summary-table-sum-row-slider'
+                                                        ),
+                                                        html.P(
+                                                            id='settings-shape-props-header-end-col-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-shape-props-header-end-col-text'
+                                                        ),
+                                                        generate_slider(
+                                                            'settings-shape-props-header-end-col-slider'
+                                                        ),
+
+                                                    ],
+                                                    style=left_output_area_style
+                                                ),
+                                                html.Div(
+                                                    id='separator-settings-output-area',
+                                                    style=separator_area_style
+                                                ),
+                                                html.Div(
+                                                    id='right-settings-output-area',
+                                                    children=[
+                                                        html.H4(
+                                                            'Data Naming Definitions',
+                                                            id='settings-names'),
+                                                        html.P(
+                                                            id='settings-names-sheet-name-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-names-sheet-name-text'
+                                                        ),
+                                                        dcc.Input(
+                                                            id='settings-names-sheet-name-input',
+                                                            type='text',
+                                                            placeholder='input text',
+                                                            debounce=True
+                                                        ),
+                                                        html.P(
+                                                            id='settings-names-country-col-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-names-country-col-text'
+                                                        ),
+                                                        dcc.Input(
+                                                            id='settings-names-country-col-input',
+                                                            type='text',
+                                                            placeholder='input text',
+                                                            debounce=True
+                                                        ),
+                                                        html.P(
+                                                            id='settings-names-store-col-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-names-store-col-text'
+                                                        ),
+                                                        dcc.Input(
+                                                            id='settings-names-store-col-input',
+                                                            type='text',
+                                                            placeholder='input text',
+                                                            debounce=True
+                                                        ),
+                                                        html.P(
+                                                            id='settings-names-main-cols-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-names-main-cols-text'
+                                                        ),
+                                                        dcc.Dropdown(
+                                                            id='settings-names-main-cols-dropdown',
+                                                            multi=True
+                                                        ),
+                                                        html.P(
+                                                            id='settings-names-entity-list-text'
+                                                        ),
+                                                        generate_hover_text(
+                                                            'settings-names-entity-list-text'
+                                                        ),
+                                                        dcc.Dropdown(
+                                                            id='settings-names-entity-list-dropdown',
+                                                            multi=True
                                                         )
-                                                    ]
+                                                    ],
+                                                    style=right_output_area_style
                                                 )
                                             ]
                                         )
@@ -540,20 +703,114 @@ def display_props_batch_filename(props_batch_content, props_batch_filename):
     else:
         return upload_default_text
 
+
 @app.callback(
-    Output('settings-shape-main-header-row-text', 'children'),
-    [Input('settings-shape-main-header-row-slider', 'value')]
+    [
+        Output('settings-names-main-cols-dropdown', 'options'),
+        Output('settings-names-entity-list-dropdown', 'options')
+    ],
+    [
+        Input('settings-names-country-col-input', 'value'),
+        Input('settings-names-store-col-input', 'value')
+    ]
 )
-def update_settings(main_header_row):
+def update_settings_dropdown(country_col, store_col):
+    global entity_list
+    if country_col is not None and store_col is not None:
+        main_cols = [{'label': x, 'value': x} for x in [country_col, store_col]]
+        entities = [{'label': x, 'value': x} for x in entity_list]
+        return main_cols, entities
+    else:
+        return [], []
+
+@app.callback(
+    [
+        Output('settings-shape-main-header-row-text', 'children'),
+        Output('settings-shape-props-header-start-col-text', 'children'),
+        Output('settings-shape-props-header-tally-first-text', 'children'),
+        Output('settings-shape-no-summary-table-rows-text', 'children'),
+        Output('settings-shape-summary-table-sum-row-text', 'children'),
+        Output('settings-shape-props-header-end-col-text', 'children'),
+        Output('settings-names-sheet-name-text', 'children'),
+        Output('settings-names-country-col-text', 'children'),
+        Output('settings-names-store-col-text', 'children'),
+        Output('settings-names-main-cols-text', 'children'),
+        Output('settings-names-entity-list-text', 'children')
+     ],
+    [
+        Input('settings-shape-main-header-row-slider', 'value'),
+        Input('settings-shape-props-header-start-col-slider', 'value'),
+        Input('settings-shape-props-header-tally-first-slider', 'value'),
+        Input('settings-shape-no-summary-table-rows-slider', 'value'),
+        Input('settings-shape-summary-table-sum-row-slider', 'value'),
+        Input('settings-shape-props-header-end-col-slider', 'value'),
+        Input('settings-names-sheet-name-input', 'value'),
+        Input('settings-names-country-col-input', 'value'),
+        Input('settings-names-store-col-input', 'value'),
+        Input('settings-names-main-cols-dropdown', 'value'),
+        Input('settings-names-entity-list-dropdown', 'value')
+    ]
+)
+def update_settings(main_header_row, props_header_start_col, props_header_tally_first, no_summary_table_rows, 
+                    summary_table_sum_row, props_header_end_col, sheet_name, country_col, store_col, main_cols, entity_list):
     # load settings
     global settings
+    
     # define texts
     main_header_row_text = 'SkipRows to Main Header'
+    props_header_start_col_text = 'SkipCols to First Props Column'
+    props_header_tally_first_text = 'Number of First X Props Columns to Tally'
+    no_summary_table_rows_text = 'Number of Rows of Summary Table'
+    summary_table_sum_row_text = 'Summary Table Sum Row Number '
+    props_header_end_col_text = 'Number of Last X Props Columns to Ignore'
+    sheet_name_text = 'Sheet to Read From'
+    country_col_text = 'Column name of Country'
+    store_col_text = 'Column name of Stores'
+    main_cols_text = 'Main Columns'
+    entity_list_text = 'Entities to find default sheet name'
+    
+    # update texts
+    if main_header_row is not None:
+        main_header_row_text += ': %s'%main_header_row
+    if props_header_start_col is not None:
+        props_header_start_col_text += ': %s'%props_header_start_col
+    if props_header_tally_first is not None:
+        props_header_tally_first_text += ': %s'%props_header_tally_first
+    if no_summary_table_rows is not None:
+        no_summary_table_rows_text += ': %s'%no_summary_table_rows
+    if summary_table_sum_row is not None:
+        summary_table_sum_row_text += ': %s'%summary_table_sum_row
+    if props_header_end_col is not None:
+        props_header_end_col_text += ': %s'%props_header_end_col
+    if sheet_name is not None:
+        sheet_name_text += ': %s'%sheet_name
+    if country_col is not None:
+        country_col_text += ': %s'%country_col
+    if store_col is not None:
+        store_col_text += ': %s'%store_col
+    if main_cols is not None:
+        main_cols_text += ': %s'%main_cols
+    if entity_list is not None:
+        entity_list_text += ': %s'%entity_list
+    
     # update settings (no output)
     if len(settings) > 0:
         settings['shape']['main_header_row'] = main_header_row
+        settings['shape']['props_header_start_col'] = props_header_start_col
+        settings['shape']['props_header_tally_first'] = props_header_tally_first
+        settings['shape']['no_summary_table_rows'] = no_summary_table_rows
+        settings['shape']['summary_table_sum_row'] = summary_table_sum_row
+        settings['shape']['props_header_end_col'] = props_header_end_col
+        settings['names']['sheet_name'] = sheet_name
+        settings['names']['country_col'] = country_col
+        settings['names']['store_col'] = store_col
+        settings['names']['main_cols'] = main_cols
+        settings['names']['entity_list'] = entity_list
+        
     # return texts
-    return main_header_row_text
+    return main_header_row_text, props_header_start_col_text, props_header_tally_first_text, no_summary_table_rows_text, \
+           summary_table_sum_row_text, props_header_end_col_text, sheet_name_text, country_col_text, store_col_text, \
+           main_cols_text, entity_list_text
 
 @app.callback(
     Output('settings-placeholder', 'children'),
@@ -572,7 +829,19 @@ def save_settings(save_settings_clicks):
     return None
 
 @app.callback(
-    Output('settings-shape-main-header-row-slider', 'value')
+    [
+        Output('settings-shape-main-header-row-slider', 'value'),
+        Output('settings-shape-props-header-start-col-slider', 'value'),
+        Output('settings-shape-props-header-tally-first-slider', 'value'),
+        Output('settings-shape-no-summary-table-rows-slider', 'value'),
+        Output('settings-shape-summary-table-sum-row-slider', 'value'),
+        Output('settings-shape-props-header-end-col-slider', 'value'),
+        Output('settings-names-sheet-name-input', 'value'),
+        Output('settings-names-country-col-input', 'value'),
+        Output('settings-names-store-col-input', 'value'),
+        Output('settings-names-main-cols-dropdown', 'value'),
+        Output('settings-names-entity-list-dropdown', 'value')
+    ]
     ,
     [
         Input('settings-load-button', 'n_clicks'),
@@ -597,9 +866,20 @@ def load_settings(load_settings_clicks, load_default_settings_clicks):
     print('[Status]', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), ' Loaded settings: ', values)
     if values is not None:
         settings = values
-        return settings['shape']['main_header_row']
+        return \
+            settings['shape']['main_header_row'],\
+            settings['shape']['props_header_start_col'], \
+            settings['shape']['props_header_tally_first'], \
+            settings['shape']['no_summary_table_rows'], \
+            settings['shape']['summary_table_sum_row'], \
+            settings['shape']['props_header_end_col'], \
+            settings['names']['sheet_name'], \
+            settings['names']['country_col'], \
+            settings['names']['store_col'], \
+            settings['names']['main_cols'], \
+            settings['names']['entity_list']
     else:
-        return None
+        return None, None, None, None, None, None, None, None, None, [], []
 
 
 @app.callback(
